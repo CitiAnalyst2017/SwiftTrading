b22328ff76e3ae48083daf9987f8a7484e20d09d
Zhuangxiaoda <zzzxxxddd@foxmail.com>
2017-08-10 19:07:59 +0800

    maybe final

--- SwiftTrading/src/main/webapp/js/Controller/mtCtrl/ptradelistctrl.js
+++ SwiftTrading/src/main/webapp/js/Controller/mtCtrl/ptradelistctrl.js
@@ -18,7 +18,7 @@ app.controller('ptradelistCtrl',function($scope,$http,$interval,$rootScope){
 	};
 
 	// mock data
-/*	$scope.porders = [{
+	/*$scope.porders = [{
 		"code":"app",
 		"position":"long",
 		"quantity":214,
@@ -26,7 +26,8 @@ app.controller('ptradelistCtrl',function($scope,$http,$interval,$rootScope){
 		"nowprice":103,
 		"profit":214,
 		"status":"CREATED",
-		"starttime":"2016-3-17"
+		"starttime":"2016-3-17",
+		"type":"sdf"
 	},{
 		"code":"mic",
 		"position":"short",
@@ -35,9 +36,10 @@ app.controller('ptradelistCtrl',function($scope,$http,$interval,$rootScope){
 		"nowprice":103,
 		"profit":-130,
 		"status":"OPEN",
-		"starttime":"2017-6-17"
-	}];*/
-
+		"starttime":"2017-6-17",
+		"type":"MARKET"
+	}];
+*/
 	$http({
 		method:'GET',
 		url:pending_order_url,
@@ -68,18 +70,37 @@ app.controller('ptradelistCtrl',function($scope,$http,$interval,$rootScope){
 	},2000);
 
 	$scope.opert = function(){
-		this.porder.status = 'CLOSING';
+
+		if(this.porder.type != "MARKET"){
+			this.porder.status = 'CLOSING';
+			$http({
+				method:'PUT',
+				url:url_prefix + '/trade/',
+				data:this.porder,
+			}).success(function(){
+				this.oper = "ing";
+				this.opdis = true;
+				alert("operate successfully!");
+			}).error(function(){
+				alert("InternetError");
+			});
+		}else{
+			$('#sello').modal('show');
+		}
+				
+	};
+
+	$scope.sellm = function(){
+		this.porder.sellprice = $scope.sellpr;
 		$http({
-			method:'PUT',
+			method:'POST',
 			url:url_prefix + '/trade/',
 			data:this.porder,
 		}).success(function(){
-			this.oper = "ing";
-			this.opdis = true;
-			alert("operate successfully!");
+
 		}).error(function(){
 			alert("InternetError");
-		});		
+		});
 	};
 
 });
--- SwiftTrading/src/main/webapp/js/Controller/stCtrl/strlistctrl.js
+++ SwiftTrading/src/main/webapp/js/Controller/stCtrl/strlistctrl.js
@@ -1,19 +1,24 @@
 
 
 app.controller('strlistCtrl',function($scope,$http,$interval,$timeout){
+
 	var str_url = url_prefix + '';
+
+	var det_url = 'a/b';
+
 	$scope.errormsg = false;
 
-	/*$http({
+	$http({
 			method:'GET',
 			url:str_url
 		}).success(function(data){
+			$scope.errormsg = false;
 			$scope.strates = data;
 		}).error(function(){
 			$scope.errormsg = true;
-	});*/
+	});
 
-	$scope.strates = [{
+	/*$scope.strates = [{
 		"id":"2412",
 		"name":"asfa",
 		"status":"running"
@@ -21,7 +26,7 @@ app.controller('strlistCtrl',function($scope,$http,$interval,$timeout){
 		"id":"122",
 		"name":"vfg",
 		"status":"finished"
-	}];
+	}];*/
 
 	/*$scope.strate = {
 		"id":"2412",
@@ -54,6 +59,17 @@ app.controller('strlistCtrl',function($scope,$http,$interval,$timeout){
 	};
 
 	$scope.showdetail = function(){
-		/*alert("asd");*/
+		$('#strd').modal('show');
+		$interval(function(){
+			$http({
+			method:'GET',
+			url:det_url,
+		}).success(function(data){
+			$scope.sdts = data;
+		}).error(function(){
+			alert("InternetError");
+		});
+		},2000);	
 	};
+
 });
